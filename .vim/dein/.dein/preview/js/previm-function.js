ca84ce7574107e3aa52083fa88e1916f4990c7ed
piyo56 <piyo56.net@gmail.com>
2016-07-23 17:37:38 +0900

    [Add].vimディレクトリを追加

---
+++ .vim/dein/.dein/preview/js/previm-function.js
@@ -0,0 +1,19 @@
+function isShowHeader() {
+return 1;
+}
+
+function getFileName() {
+return "/Users/naka/Desktop/github/elevation/README.md";
+}
+
+function getFileType() {
+return "markdown";
+}
+
+function getLastModified() {
+return "2016/07/23 (土) 17:21:54";
+}
+
+function getContent() {
+return "# elevation\nGoogleMapのAPI(GMaps.js)を使った簡単なwebサイト。経路に沿った標高を算出して高低差を可視化する。\n\n## TO DO:\n\n- 出発・目的地が見つからない／ルートが一つもないなどのAPI側のエラーをページ側で表示する\n- ルートを切り替えられる機能\n- ルート毎に高低差がわかるような描画";
+}
