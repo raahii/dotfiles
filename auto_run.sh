fad9bbd6c47af43b8c150833ea4513a9469e0c30
piyo56 <piyo56.net@gmail.com>
2016-08-18 00:27:13 +0900

    [Update] auto_run.shにCentOS向けのvimインストール処理を実装

--- auto_run.sh
+++ auto_run.sh
@@ -3,42 +3,35 @@
 #require:
 #   - wget or git
 
-DOT_DIRECTORY="${HOME}/dotfiles"
-#DOT_TARBALL="https://github.com/piyo56/dotfiles/tarball/master"
-#REMOTE_URL="git@github.com:piyo56/dotfiles.git"
-#
-## ディレクトリがなければダウンロード（と解凍）する
-#if [ ! -d ${DOT_DIRECTORY} ]; then
-#  echo "Downloading dotfiles..."
-#  mkdir ${DOT_DIRECTORY}
-#
-#  if type "git" > /dev/null 2>&1; then
-#    git clone --recursive "${REMOTE_URL}" "${DOT_DIRECTORY}"
-#  else
-#    wget -fsSLo ${HOME}/dotfiles.tar.gz ${DOT_TARBALL}
-#    tar -zxf ${HOME}/dotfiles.tar.gz --strip-components 1 -C ${DOT_DIRECTORY}
-#    rm -f ${HOME}/dotfiles.tar.gz
-#  fi
-#
-#  echo $(tput setaf 2)Download dotfiles complete!. ✔︎ $(tput sgr0)
-#fi
-
-# deploy
-# ln -s .vimrc $HOME/.vimrc
-# ln -s .zshrc $HOME/.zshrc
-
-# dein.vim install
-mkdir -p ~/.vim/dein/repos/github.com/Shougo/dein.vim
-wget https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > installer.sh
-sh ./installer.sh $HOME/.vim/dein/repos/github.com/Shougo/dein.vim
-
-# zsh install
-ZSH_REMOTE_URL="https://github.com/zsh-users/zsh.git"
-ZSH_TARBALL="https://github.com/zsh-users/zsh/tarball/master"
-if type "git" > /dev/null 2>&1; then
-    git clone --recursive "${ZSH_REMOTE_URL}" "${DOT_DIRECTORY}"
+# OS判定してOSごとに別の処理を行う（予定）
+if [ "$(uname)" == 'Darwin' ]; then
+    OS='Mac'
+elif [ "$(expr substr $(uname -s) 1 5)" == 'Linux' ]; then
+    declare -a info=($(./sh/get_linux_dist.sh))
+    OS=${info[0]}
+elif [ "$(expr substr $(uname -s) 1 10)" == 'MINGW32_NT' ]; then                                                                                           
+    OS='Cygwin'
 else
-    wget -fsSLo ${HOME}/zsh.tar.gz ${ZSH_TARBALL}
-    tar -zxf ${HOME}/zsh.tar.gz --strip-components 1 -C ${DOT_DIRECTORY}
-    rm -f ${HOME}/zsh.tar.gz
+    echo "Your platform ($(uname -a)) is not supported."
+    exit 1
+fi
+
+#install vim 7.3 to enable clipboard :)
+if [ ! "$(which vim)" ]; then
+    echo -e "\n-----------------[ installing vim ]-----------------\n"
+    if [ "$OS" == 'redhat' ]; then
+        # clipboard有効に必要(多分)
+        sudo yum install -y mercurial ncurses-devel make gcc
+        sudo yum install -y gtk2-devel atk-devel libX11-devel libXt-devel
+
+        # ダウンロードしてくるのに数分かかる
+        cd /usr/local/src
+        sudo hg clone https://bitbucket.org/vim-mirror/vim vim
+        cd vim
+ 
+        # コンパイル＆インストール
+        sudo ./configure --with-features=huge --enable-multibyte --disable-selinux
+        sudo make
+        sudo make install
+    fi
 fi
