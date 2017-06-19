793037683a689826248bb7047a3b05cdd870aa1a
piyo56 <piyo56.net@gmail.com>
2017-06-19 18:02:48 +0900

    peco-historyのスクリプトをLinuxに対応

--- .zsh/peco-sources/history.zsh
+++ .zsh/peco-sources/history.zsh
@@ -1,8 +1,14 @@
 setopt hist_ignore_all_dups
 
 function peco_select_history() {
+  if type tac 2>/dev/null 1>/dev/null 
+  then
+    BUFFER=$(fc -l -n 1 | tac | uniq | peco --query "$LBUFFER")
+  else
     BUFFER=$(fc -l -n 1 | tail -r | uniq | peco --query "$LBUFFER")
-    CURSOR=$#BUFFER         # move cursor
-    zle -R -c               # refresh
+  fi
+
+  CURSOR=$#BUFFER         # move cursor
+  zle -R -c               # refresh
 }
 zle -N peco_select_history
