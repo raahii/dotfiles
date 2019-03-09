5b657be0926ae68fb5ec7b71b3691289c0c04fbc
raahii <piyo56.net@gmail.com>
2019-03-09 20:42:51 +0900

    fix fish functions

--- fish/functions/peco_change_directory.fish
+++ fish/functions/peco_change_directory.fish
@@ -4,7 +4,7 @@ function peco_change_directory
     echo $HOME/Desktop
     echo $HOME/.config
     ls -ad */|perl -pe "s#^#$PWD/#"|egrep -v "^$PWD/\."|head -n 5
-    sort -r -t '|' -k 3 ~/.z|sed -e 's/\|.*//'
+    sort -r -t '|' -k 3 $Z_DATA | sed -e 's/\|.*//'
     ghq list -p
     ls -ad */|perl -pe "s#^#$PWD/#"|grep -v \.git
   end | sed -e 's/\/$//' | awk '!a[$0]++' | _peco_change_directory $argv
