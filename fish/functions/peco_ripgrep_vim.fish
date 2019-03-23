00790154f2d8ae9f3494c208bed4f74a6badc653
raahii <piyo56.net@gmail.com>
2019-03-24 05:36:11 +0900

    fix peco_ripgrep_vim.fish

--- fish/functions/peco_ripgrep_vim.fish
+++ fish/functions/peco_ripgrep_vim.fish
@@ -1,5 +1,5 @@
 function peco_ripgrep_vim
   vared __query
-  rg -n $__query | peco | awk -F : '{print "-c" " " $2 " " $1}' | xargs vim
+  rg -n $__query | peco | awk -F : '{print "-c" " " $2 " " $1}' | xargs nvim
   set __query ""
 end
