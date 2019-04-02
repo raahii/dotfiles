e355eaca244d0e62d3793592e58669e6ea65342c
raahii <piyo56.net@gmail.com>
2019-04-02 19:42:52 +0900

    add alias pbcopy/pbpaste for linux

--- fish/config_linux.fish
+++ fish/config_linux.fish
@@ -2,3 +2,6 @@ set -x CLICOLOR 1
 set -x LANG en_US.UTF-8
 set -x LC_ALL en_US.UTF-8
 set -x LC_CTYPE en_US.UTF-8
+
+alias pbcopy "xsel --clipboard --input"
+alias pbpaste "xsel --clipboard --output"
