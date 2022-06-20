f4e3aaa8977d54f2d8837c6ae9469a644f0709b8
raahii <piyo56.net@gmail.com>
2022-06-20 23:24:10 +0900

    Update *env settings.

--- fish/config_langs.fish
+++ fish/config_langs.fish
@@ -8,28 +8,28 @@ if type go > /dev/null 2>&1
   end
 end
 
-if [ -e "$HOME/.goenv" ]
+if type goenv > /dev/null 2>&1
   set -x GOENV_ROOT $HOME/.goenv
   set -x PATH $GOENV_ROOT/bin $PATH
   eval (goenv init - --no-rehash | source)
 end
 
 # python
-if [ -e "$HOME/.pyenv" ]
+if type pyenv > /dev/null 2>&1
   set -x PYENV_ROOT $HOME/.pyenv
   set -x PATH $PYENV_ROOT/bin $PATH
   status is-login; and pyenv init --path | source
 end
 
 # ruby
-if [ -e "$HOME/.rbenv" ]
+if type rbenv > /dev/null 2>&1
   set -x RBENV_ROOT $HOME/.rbenv
   set -x PATH $RBENV_ROOT/bin $PATH
   source (rbenv init - --no-rehash| psub)
 end
 
 # node
-if [ -e "$HOME/.nodenv" ]
+if type nodenv > /dev/null 2>&1
   set -x fish_user_paths $HOME/.nodenv/bin $fish_user_paths
   status --is-interactive; and source (nodenv init -|psub)
 end
